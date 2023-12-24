0308820e3a7da44407e1ba41d983314d61b6c4e9
acyshoang <trunghoang1124@gmail.com>
2023-12-24 23:46:39 +0700

    updated

--- enchantplus/src/main/java/com/enchantingplus/Enchanting.java
+++ enchantplus/src/main/java/com/enchantingplus/Enchanting.java
@@ -1,5 +1,21 @@
 package com.enchantingplus;
 
-public class Enchanting {
-    
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Enchanting implements CommandExecutor{
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // Call "enct" as a command
+        if (command.getName().equalsIgnoreCase("enct")) {
+            
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("Only Player Can You Use This Command");
+            }
+        }
+        return true;
+    }
 }
