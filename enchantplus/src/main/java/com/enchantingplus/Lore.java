d49805cb62e8d9e397959b0f49987c055e2a41f7
acyshoang <trunghoang1124@gmail.com>
2023-12-27 20:55:44 +0700

    updated

--- enchantplus/src/main/java/com/enchantingplus/Lore.java
+++ enchantplus/src/main/java/com/enchantingplus/Lore.java
@@ -1,33 +1,44 @@
 package com.enchantingplus;
 
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class Lore implements CommandExecutor{
+    private List<String> lore;
+
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player)sender;
-        // String loreItem = args[0];
+        
         if (!(sender instanceof Player)) {
             return false;
         }
 
         if (command.getName().equalsIgnoreCase("setlore")) {
-            player.sendMessage("This is setlore command");
+            ItemStack item = player.getInventory().getItemInMainHand();
+
+            if (item == null) {
+                player.sendMessage("You aren't holding an item");
+                return true;
+            }
+
+            ItemMeta meta = item.getItemMeta();
+
+            String loreline = args[0];
+
+            lore.add(loreline);
+            meta.setLore(lore);
+            item.setItemMeta(meta);
+
+            player.sendMessage("Lore added successfully to item");
+
             return true;
         }
-
-        if (command.getName().equalsIgnoreCase("addlore")) {
-            player.sendMessage("This is addlore command");
-            return true;
-        }
-
-        if (command.getName().equalsIgnoreCase("rename")) {
-            player.sendMessage("This is rename command");
-            return true;
-        }
-
         return true;
     }
 }
--- enchantplus/src/main/java/com/enchantingplus/Plugin.java
+++ enchantplus/src/main/java/com/enchantingplus/Plugin.java
@@ -2,32 +2,26 @@ package com.enchantingplus;
 
 import java.util.logging.Logger;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /*
  * enchantplus java plugin
  */
-public class Plugin extends JavaPlugin
-{
-  private static final Logger LOGGER=Logger.getLogger("enchantplus");
+public class Plugin extends JavaPlugin {
+  private static final Logger LOGGER = Logger.getLogger("enchantplus");
   private Enchanting enchant;
   private Lore lore;
 
-  public void onEnable()
-  {
+  public void onEnable() {
     LOGGER.info("enchantplus-devbuild-0.2 enabled");
     enchant = new Enchanting();
     lore = new Lore();
+
     getCommand("enct").setExecutor(enchant);
     getCommand("setlore").setExecutor(lore);
-    getCommand("addlore").setExecutor(lore);
-    getCommand("rename").setExecutor(lore);
-    
   }
 
-  public void onDisable()
-  {
+  public void onDisable() {
     LOGGER.info("enchantplus-devbuild-0.2 disabled");
   }
 }
